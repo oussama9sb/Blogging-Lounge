44dad8f308f1f91cfa064e17b4b9e546f00c88e8
lenovo <oussamakssibi1@outlook.fr>
2025-04-23 17:50:56 +0100

    feat: studio page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
-  // const projectId = process.env;
-  // console.log("projectId", projectId);
   return (
     <main>
       <Hero />
